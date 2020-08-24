7a63322a9b7e711bf543d82835fdb89e6cd026ff
Mera Youssef <merayoussef91@hotmail.com>
2020-08-24 17:28:50 -0500

    activated add task button

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+
+var createTaskHandler = function() {
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task.";
+    tasksToDoE1.appendChild(listItemE1);
+}
+
+buttonEl.addEventListener("click", createTaskHandler); {
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task.";
+    tasksToDoE1.appendChild(listItemE1);
+};
+
